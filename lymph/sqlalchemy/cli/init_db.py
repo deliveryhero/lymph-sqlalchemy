dadeb6d0e844ae13e06aa207bb2980a92e564931
Andrey Bilunyk <andrey.bilunyk@deliveryhero.com>
2016-03-01 15:04:29 +0100

    Fix issue with drop_all for init_db command for CASCADE deletion case

--- lymph/sqlalchemy/cli/init_db.py
+++ lymph/sqlalchemy/cli/init_db.py
@@ -1,3 +1,12 @@
+from sqlalchemy.engine import reflection
+from sqlalchemy.schema import (
+    MetaData,
+    Table,
+    DropTable,
+    ForeignKeyConstraint,
+    DropConstraint,
+)
+
 from alembic.util import CommandError as AlembicCommandError
 from lymph.cli.base import Command
 
@@ -20,7 +29,7 @@ class InitDB(Command):
     def run(self):
         self.store = self.config.get_dependency('store')
         if self.args.get('--clear'):
-            self.store.drop_all()
+            self.drop_everything()
         self.store.create_all()
         self.handle_migrations()
 
@@ -41,3 +50,42 @@ class InitDB(Command):
             pass
 
         alembic.stamp()
+
+    def drop_everything(self):
+        """
+        Recipe method for dropping all tables in case of circular constraints dependencies
+        https://bitbucket.org/zzzeek/sqlalchemy/wiki/UsageRecipes/DropEverything
+        """
+        conn = self.store.engine.connect()
+
+        trans = conn.begin()
+
+        inspector = reflection.Inspector.from_engine(self.store.engine)
+
+        # gather all data first before dropping anything.
+        # some DBs lock after things have been dropped in
+        # a transaction.
+
+        metadata = MetaData()
+
+        tbs = []
+        all_fks = []
+
+        for table_name in inspector.get_table_names():
+            fks = []
+            for fk in inspector.get_foreign_keys(table_name):
+                if not fk['name']:
+                    continue
+                fks.append(ForeignKeyConstraint((),(),name=fk['name']))
+            t = Table(table_name,metadata,*fks)
+            tbs.append(t)
+            all_fks.extend(fks)
+
+        for fkc in all_fks:
+            conn.execute(DropConstraint(fkc))
+
+        for table in tbs:
+            conn.execute(DropTable(table))
+
+        trans.commit()
+
